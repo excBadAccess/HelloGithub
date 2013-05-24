ccf6336636c478e5e83d2a11629e64f2529d22ff
Alex Lee <jianguo.lee@qq.com>
2013-05-24 14:21:52 +0800

    create project

---
+++ AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  HelloGithub
+//
+//  Created by lee on 13-5-24.
+//  Copyright (c) 2013年 lee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  HelloGithub
+//
+//  Created by lee on 13-5-24.
+//  Copyright (c) 2013年 lee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
